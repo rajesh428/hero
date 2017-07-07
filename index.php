058c447873f0941b6b20f05a67a5bca4daebfba2
rajesh428 <nittururajesh2@gmail.com>
2017-07-07 18:13:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 // Merchant key here as provided by Payu
-$MERCHANT_KEY = "JBZaLc";
+$MERCHANT_KEY = "gtKFFx";
 
 // Merchant Salt as provided by Payu
-$SALT = "GQs7yium";
+$SALT = "eCwWELxi";
 
 // End point - change to https://secure.payu.in for LIVE mode
 $PAYU_BASE_URL = "https://test.payu.in";
